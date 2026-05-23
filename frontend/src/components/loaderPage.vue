7e0a4b8da39357f8c22fdbd948a21e56d38cabb8
Miguel00Roza <miguelroza13@gmail.com>
2026-05-23 17:01:22 -0300

    feat: adição de skeletons de loading - loaderPage baseado no appLayout - loaderCard generico

---
+++ frontend/src/components/loaderPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="w-screen h-screen p-5 flex">
+    
+    <!-- ASIDE -->
+    <div class="w-67 h-full bg-gray-200 animate-pulse"></div>
+
+    <div class="flex flex-col flex-1 ml-5">
+      <!-- HEADER -->
+      <div class="h-20 bg-gray-200 animate-pulse"></div>
+
+      <!-- MAIN -->
+      <div class="h-full bg-gray-200 animate-pulse m-5"></div>
+
+    </div>
+  </div>
+</template>
