12b51a60f6e96ffb32d3dcc4ab90cc03a81cbe44
arthurlanz <arthurlanznaster@gmail.com>
2026-05-29 15:12:17 -0300

    feat: buscar dados de membros autenticados após o login

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -1,27 +1,98 @@
-import { defineStore } from 'pinia';
+import { defineStore } from 'pinia'
+
+import api from '@/api'
+
+interface User {
+  id: string
+  usuario: string
+  usuario_nome: string
+  usuario_username: string
+  curso: string
+  semestre: number
+  squad: string
+}
 
 interface AuthState {
-  token: string | null;
-  user: null | null; // Tipar com a interface do Usuario
+  accessToken: string | null
+  refreshToken: string | null
+  user: User | null
 }
 
 export const useAuthStore = defineStore('auth', {
+
   state: (): AuthState => ({
-    token: localStorage.getItem('token') || null,
-    user: null,
+    accessToken: localStorage.getItem('accessToken'),
+    refreshToken: localStorage.getItem('refreshToken'),
+    user: null
   }),
+
   getters: {
-    isAuthenticated: (state) => !!state.token,
+    isAuthenticated: (state) => !!state.accessToken
   },
+
   actions: {
-    setToken(token: string) {
-      this.token = token;
-      localStorage.setItem('token', token);
+
+    async login(
+      username: string,
+      password: string
+    ) {
+
+      const response = await api.post(
+        '/token/',
+        {
+          username,
+          password
+        }
+      )
+
+      const { access, refresh } = response.data
+
+      this.setTokens(
+        access,
+        refresh
+      )
+
+      await this.fetchUser()
+
+      return response.data
     },
+
+    async fetchUser() {
+
+      const response = await api.get(
+        '/api/membros/me/'
+      )
+
+      this.user = response.data
+    },
+
+    setTokens(
+      accessToken: string,
+      refreshToken: string
+    ) {
+
+      this.accessToken = accessToken
+      this.refreshToken = refreshToken
+
+      localStorage.setItem(
+        'accessToken',
+        accessToken
+      )
+
+      localStorage.setItem(
+        'refreshToken',
+        refreshToken
+      )
+    },
+
     logout() {
-      this.token = null;
-      this.user = null;
-      localStorage.removeItem('token');
+
+      this.accessToken = null
+      this.refreshToken = null
+      this.user = null
+
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken')
     }
   }
-});
+})
